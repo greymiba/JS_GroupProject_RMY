c8c11303795fe8956335542b92d4aa77202e1a35
greymiba <greymiba@gmail.com>
2019-12-11 10:34:04 -0800

    Combined generic puzzle size logic with splash page.

--- PicPuzzle.js
+++ PicPuzzle.js
@@ -1,119 +1,101 @@
 'use strict';
+
+//difficulty & category variables hold the user-selected puzzle difficulty and picture category.
+let difficulty, category;
+
 let canvas1;
 let ctx1;
-let iWidth;
-let iHeight;
+let iWidth; // variable for image width
+let iHeight; // variable for image height
 
-let tileDivisor;
-let tileDimArray;
-let ulx;
-let uly;
-let umx;
-let umy;
-let urx;
-let ury;
+let tileDivisor; // image to be divided into tileDivisor*tileDivisor sections
+let tileDimArray; // array of tile dimensions, [tile width, tile height]
+let tilePosArray2 = [];
 
-let mlx;
-let mly;
-let mmx;
-let mmy;
-let mrx;
-let mry;
+// let tilePosArray;
 
-let llx;
-let lly;
-let lmx;
-let lmy;
-let lrx;
-let lry;
+function buildArray() {
+	tilePosArray2 = [];
 
-let tilePosArray;
+	for (let k = 0; k < difficulty; k++) {
+		console.log(`k:${k}`);
+		for (let m = 0; m < difficulty; m++) {
+			console.log(`m:${m}`);
+			tilePosArray2[k * difficulty + m] = {
+				x0: m * tileDimArray[0],
+				y0: k * tileDimArray[1],
+				xCanvas: 0,
+				yCanvas: 0
+			};
+			console.log(tilePosArray2[k * difficulty + m]);
+		}
+	}
+}
 
 function initialize() {
-  const canvas1 = document.getElementById('canvas1');
-  const ctx1 = canvas1.getContext('2d');
-  let rhinoImg = new Image(300, 227);
-  rhinoImg.src = 'https://previews.123rf.com/images/jonnysek/jonnysek1209/jonnysek120900076/15301004-way-in-the-park-as-nice-autumn-background.jpg';
-  // rhinoImg.src = 'https://mdn.mozillademos.org/files/5397/rhino.jpg';
+	const canvas1 = document.getElementById('canvas1');
+	const ctx1 = canvas1.getContext('2d');
+	let rhinoImg = new Image(300, 227);
+	rhinoImg.src = 'https://mdn.mozillademos.org/files/5397/rhino.jpg';
+	// rhinoImg.src = 'https://mdn.mozillademos.org/files/5397/rhino.jpg';
+	let form = document.getElementById('form');
+	form.addEventListener('submit', validateButton, false);
 
-  iWidth = rhinoImg.width;
-  iHeight = rhinoImg.height;
+	iWidth = rhinoImg.width;
+	iHeight = rhinoImg.height;
+	console.log(`**Debug: difficulty = ${difficulty}`);
 
-  tileDivisor = 3;
-  tileDimArray = [Math.floor(iWidth / tileDivisor), Math.floor(iHeight / tileDivisor)];
-  canvas1.width = iWidth;
-  canvas1.height = iHeight;
-  canvas1.style.border = '1px solid black';
+	tileDivisor = difficulty ? difficulty : 3;
+	tileDimArray = [ Math.floor(iWidth / tileDivisor), Math.floor(iHeight / tileDivisor) ];
+	canvas1.width = iWidth;
+	canvas1.height = iHeight;
+	canvas1.style.border = '1px solid red';
 
+	if (tilePosArray2.length !== 0) {
+		buildArray();
+	}
 
-  ulx = 0;
-  uly = 0;
-  umx = tileDimArray[0];
-  umy = 0;
-  urx = tileDimArray[0] * 2;
-  ury = 0;
+	rhinoImg.addEventListener('load', (e) => {
 
-  mlx = 0;
-  mly = tileDimArray[1];
-  mmx = tileDimArray[0];
-  mmy = tileDimArray[1];
-  mrx = tileDimArray[0] * 2;
-  mry = tileDimArray[1];
+    //////Randomize array.
+		for (let k = 0; k < tilePosArray2.length; k++) {
+			ctx1.drawImage(
+				rhinoImg,
+				tilePosArray2[k].x0,
+				tilePosArray2[k].y0,
+				...tileDimArray,
+				tilePosArray2[tilePosArray2.length - k - 1].x0,
+				tilePosArray2[tilePosArray2.length - k - 1].y0,
+				...tileDimArray
+			);
+		}
+	});
 
-  llx = 0;
-  lly = tileDimArray[1] * 2;
-  lmx = tileDimArray[0];
-  lmy = tileDimArray[1] * 2;
-  lrx = tileDimArray[0] * 2;
-  lry = tileDimArray[1] * 2;
+	let pLog = document.getElementById('mouseCoords');
+	document.addEventListener('mousemove', logMouse);
 
-  tilePosArray = [
-    [ulx, uly],
-    [umx, umy],
-    [urx, ury],
-    [mlx, mly],
-    [mmx, mmy],
-    [mrx, mry],
-    [llx, lly],
-    [lmx, lmy],
-    [lrx, lry]
-  ];
-
-
-  rhinoImg.addEventListener('load', (e) => {
-    //ctx1.drawImage(rhinoImg,0,0,iWidth,iHeight,0,0,iWidth,iHeight);
-
-    for (let k = 0; k < tilePosArray.length; k++) {
-      ctx1.drawImage(
-        rhinoImg,
-        ...tilePosArray[k],
-        ...tileDimArray,
-        ...tilePosArray[tilePosArray.length - k - 1],
-        ...tileDimArray
-      );
-    }
-  });
-
-  let pLog = document.getElementById('mouseCoords');
-  document.addEventListener('mousemove', logMouse);
-
-  function logMouse(e) {
-    pLog.innerHTML = `Screen [X,Y]: [${e.screenX},${e.screenY}]<p> Client[X,Y]:
- [${e.clientX},${e.clientY}]</p><p>Tile Pos Array Length: ${tilePosArray.length}</p><p>Canvas width: ${canvas1.width}</p><p>Canvas height: ${canvas1.height}</p><p>Image width: ${iWidth}</p><p>Image height: ${iHeight}</p><p>Image Natural width: ${rhinoImg.naturalWidth}</p><p>Image Natural height: ${rhinoImg.naturalHeight}</p>`;
-  }
-
-} // end initialize
-
-// determines selections for difficulty and category, then displays puzzle
-function validateButton() {
-  let difficulties = document.getElementsByName('difficulty');
-  let categories = document.getElementsByName('category');
-  let difficulty, category;
-  [...difficulties].forEach(e => e.checked === true ? difficulty = e.value : null);
-  [...categories].forEach(e => e.checked === true ? category = e.value : null);
-  console.log('****', difficulty);
-  console.log('****', category);
-  // hide the intro screen, show the puzzle screen
-  introScreen.style.display = "none";
-  puzzle.style.display = "block";
+	function logMouse(e) {
+		pLog.innerHTML = `Screen [X,Y]: [${e.screenX},${e.screenY}]<p> Client[X,Y]:
+ [${e.clientX},${e.clientY}]</p><p>Tile Pos Array2 Length: ${tilePosArray2.length}</p><p>Tile Dim2: x-${tileDimArray[0]} y-${tileDimArray[1]}</p><p>Tile Pos Arr ulCoord: x-${tilePosArray2[0]
+			.x0} y-${tilePosArray2[0]
+			.y0}</p><p>Canvas width: ${canvas1.width}</p><p>Canvas height: ${canvas1.height}</p><p>Image width: ${iWidth}</p><p>Image height: ${iHeight}</p><p>Image Natural width: ${rhinoImg.naturalWidth}</p><p>Image Natural height: ${rhinoImg.naturalHeight}</p>`;
+	}
 }
+// determines selections for difficulty and catergory
+function validateButton(e) {
+	let difficulties = document.getElementsByName('difficulty');
+	let categories = document.getElementsByName('category');
+	difficulty = [ ...difficulties ].filter((p) => p.checked)[0].value;
+	category = [ ...categories ].filter((p) => p.checked)[0].value;
+	e.preventDefault();
+	buildArray();
+	initialize();
+
+	//***Note:  gameStart() after choices made??
+
+	console.log('****', difficulty);
+  console.log('****', category);
+    // hide the intro screen, show the puzzle screen
+    introScreen.style.display = "none";
+    puzzle.style.display = "block";
+} // end initialize
