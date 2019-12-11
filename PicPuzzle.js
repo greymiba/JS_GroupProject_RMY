7fcf83f6f1744b7bb08b66f304e6df427ee0ac13
greymiba <greymiba@gmail.com>
2019-12-11 11:18:03 -0800

    Larger images are properly scaled to fit in canvas.

--- PicPuzzle.js
+++ PicPuzzle.js
@@ -7,27 +7,30 @@ let canvas1;
 let ctx1;
 let iWidth; // variable for image width
 let iHeight; // variable for image height
+let oWidth;
+let oHeight;
 
 let tileDivisor; // image to be divided into tileDivisor*tileDivisor sections
-let tileDimArray; // array of tile dimensions, [tile width, tile height]
-let tilePosArray2 = [];
+let tileDimArrayScaled; // array of tile dimensions, [tile width, tile height]
+let tileDimArray0;
+let tilePosArray0 = [];
 
 // let tilePosArray;
 
 function buildArray() {
-	tilePosArray2 = [];
+	tilePosArray0 = [];
 
 	for (let k = 0; k < difficulty; k++) {
 		console.log(`k:${k}`);
 		for (let m = 0; m < difficulty; m++) {
 			console.log(`m:${m}`);
-			tilePosArray2[k * difficulty + m] = {
-				x0: m * tileDimArray[0],
-				y0: k * tileDimArray[1],
-				xCanvas: 0,
-				yCanvas: 0
+			tilePosArray0[k * difficulty + m] = {
+				x0: m * tileDimArray0[0],
+				y0: k * tileDimArray0[1],
+				xCanvas: m * tileDimArrayScaled[0],
+				yCanvas: k * tileDimArrayScaled[1]
 			};
-			console.log(tilePosArray2[k * difficulty + m]);
+			console.log(tilePosArray0[k * difficulty + m]);
 		}
 	}
 }
@@ -35,38 +38,44 @@ function buildArray() {
 function initialize() {
 	const canvas1 = document.getElementById('canvas1');
 	const ctx1 = canvas1.getContext('2d');
-	let rhinoImg = new Image(300, 227);
-	rhinoImg.src = 'https://mdn.mozillademos.org/files/5397/rhino.jpg';
+	let rhinoImg = new Image(600,454);
+	rhinoImg.src = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.nationaltrust.org.uk%2Fimages%2F1431747858549-stourhead-autumn-nov-2013-2.jpg&f=1&nofb=1';
 	// rhinoImg.src = 'https://mdn.mozillademos.org/files/5397/rhino.jpg';
 	let form = document.getElementById('form');
 	form.addEventListener('submit', validateButton, false);
 
 	iWidth = rhinoImg.width;
-	iHeight = rhinoImg.height;
+  iHeight = rhinoImg.height;
+  oWidth = rhinoImg.naturalWidth;
+  oHeight = rhinoImg.naturalHeight;
 	console.log(`**Debug: difficulty = ${difficulty}`);
 
 	tileDivisor = difficulty ? difficulty : 3;
-	tileDimArray = [ Math.floor(iWidth / tileDivisor), Math.floor(iHeight / tileDivisor) ];
+	tileDimArrayScaled = [ Math.floor(iWidth / tileDivisor), Math.floor(iHeight / tileDivisor) ];
+  tileDimArray0 = [ Math.floor(oWidth / tileDivisor), Math.floor(oHeight / tileDivisor) ];
+  
 	canvas1.width = iWidth;
 	canvas1.height = iHeight;
 	canvas1.style.border = '1px solid red';
 
-	if (tilePosArray2.length !== 0) {
+	if (tilePosArray0.length !== 0) {
 		buildArray();
 	}
 
 	rhinoImg.addEventListener('load', (e) => {
 
     //////Randomize array.
-		for (let k = 0; k < tilePosArray2.length; k++) {
+		for (let k = 0; k < tilePosArray0.length; k++) {
 			ctx1.drawImage(
 				rhinoImg,
-				tilePosArray2[k].x0,
-				tilePosArray2[k].y0,
-				...tileDimArray,
-				tilePosArray2[tilePosArray2.length - k - 1].x0,
-				tilePosArray2[tilePosArray2.length - k - 1].y0,
-				...tileDimArray
+				tilePosArray0[k].x0,
+				tilePosArray0[k].y0,
+				...tileDimArray0,
+				tilePosArray0[tilePosArray0.length - k - 1].xCanvas,
+        tilePosArray0[tilePosArray0.length - k - 1].yCanvas,
+        // tilePosArray0[k].xCanvas,
+				// tilePosArray0[k].yCanvas,
+				...tileDimArrayScaled
 			);
 		}
 	});
@@ -76,8 +85,8 @@ function initialize() {
 
 	function logMouse(e) {
 		pLog.innerHTML = `Screen [X,Y]: [${e.screenX},${e.screenY}]<p> Client[X,Y]:
- [${e.clientX},${e.clientY}]</p><p>Tile Pos Array2 Length: ${tilePosArray2.length}</p><p>Tile Dim2: x-${tileDimArray[0]} y-${tileDimArray[1]}</p><p>Tile Pos Arr ulCoord: x-${tilePosArray2[0]
-			.x0} y-${tilePosArray2[0]
+ [${e.clientX},${e.clientY}]</p><p>Tile Pos Array2 Length: ${tilePosArray0.length}</p><p>Tile Dim2: x-${tileDimArrayScaled[0]} y-${tileDimArrayScaled[1]}</p><p>Tile Pos Arr ulCoord: x-${tilePosArray0[0]
+			.x0} y-${tilePosArray0[0]
 			.y0}</p><p>Canvas width: ${canvas1.width}</p><p>Canvas height: ${canvas1.height}</p><p>Image width: ${iWidth}</p><p>Image height: ${iHeight}</p><p>Image Natural width: ${rhinoImg.naturalWidth}</p><p>Image Natural height: ${rhinoImg.naturalHeight}</p>`;
 	}
 }
